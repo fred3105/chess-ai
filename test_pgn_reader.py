6625b547f768a04073158cecaeea85cb4a8b3d61
Frédéric Legrand <frederic@phospho.ai>
2025-08-31 22:50:44 +0200

    chore: add more tests

--- test_pgn_reader.py
+++ test_pgn_reader.py
@@ -7,6 +7,7 @@ def pgn_final_position(pgn_moves: str, expected_final: np.ndarray):
     final_board = game.get_board_positions()[-1]
 
     print(final_board)
+    print(game.print_board())
 
     # We only check the non-zero squares, because piece placements
     # can vary in representation but final result must match
@@ -62,6 +63,65 @@ def test_second_pgn_final_position():
             [-1, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 2, 0, -100],
             [0, 0, 0, 0, -2, -5, 0, 0],
+        ]
+    )
+    pgn_final_position(pgn_moves, expected_final)
+
+
+def test_third_pgn_final_position():
+    pgn_moves = (
+        "1. Nf3 d5 2. c4 e6 3. g3 f5 4. Bg2 Nf6 5. O-O c6 6. d3 Bd6 7. b3 O-O 8. Bb2 "
+        "Nbd7 9. Nbd2 e5 10. e4 fxe4 11. dxe4 d4 12. Ne1 Nc5 13. b4 Ne6 14. c5 Bc7 "
+        "15. Nd3 a5 16. a3 axb4 17. axb4 Rxa1 18. Bxa1 Qe8 19. Kh1 Qg6 20. Qb3 Kh8 "
+        "21. f4 exf4 22. gxf4 Ng4 23. h3 Ne3 24. Rf2 Nxf4 25. Bf1 Qg3 26. Rf3 Qh4 "
+        "27. Nf2 Ng6 28. Rxf8+ Nxf8 29. Qf7 Qf6 30. Qxf6 gxf6 31. Bxd4 Nxf1 32. "
+        "Bxf6+ Kg8 33. Nxf1 Ne6 34. Nd3 Nf4 35. Nxf4 Bxf4 36. Kg2 Be6 37. Bc3 Kf7 "
+        "38. Bd2 Be5 39. Ne3 Ke7 40. Ng4 Bxg4 41. hxg4 Ke6 42. Kf3 Bf6 43. Ke3 Bg5+ "
+        "44. Kd3 Bf6 45. Bc3 Bg5 46. Bd4 Bf4 47. Kc4 Bc7 48. b5 Bd8 49. bxc6 bxc6 "
+        "50. Bc3 Be7 51. e5 Kd7 52. Kd4 Ke6 53. Ke4 Bd8 54. Bd2 Bc7 55. Be3 Bd8 56. "
+        "g5 Be7 57. Bf4 Bxc5 58. g6 Be7 59. Bg5 Bxg5 60. g7 h5 61. g8=Q+ Kd7 62. "
+        "Qxg5 Kc7 63. Qxh5 Kb6 64. Kd4 c5+ 65. Kc4 Kc6 66. e6 Kd6 67. Qxc5+ Kxe6 68. "
+        "Qd5+ Kf6 69. Kd4 Ke7 70. Ke4 Kf6 71. Kf4 Ke7 72. Qc6 Kf7 73. Ke5 1-0"
+    )
+
+    expected_final = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 100, 0, 0, 0],
+            [0, 0, 9, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, -100, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    pgn_final_position(pgn_moves, expected_final)
+
+
+def test_fourth_pgn_final_position():
+    pgn_moves = (
+        "1. d4 d5 2. c4 c6 3. Nc3 Nf6 4. Nf3 e6 5. b3 Nbd7 6. Qc2 Be7 7. Bd2 O-O 8. "
+        "e4 dxe4 9. Nxe4 Nxe4 10. Qxe4 Nf6 11. Qd3 c5 12. Be2 b6 13. Bc3 Bb7 14. Rd1 "
+        "cxd4 15. Bxd4 Bb4+ 16. Bc3 Qxd3 17. Rxd3 Bxc3+ 18. Rxc3 Rfd8 19. O-O Ne4 "
+        "20. Re3 Rd6 21. h4 Rad8 22. Ng5 Nd2 23. Rd1 h6 24. Nh3 Nf3+ 25. Bxf3 Rxd1+ "
+        "26. Bxd1 Rxd1+ 27. Kh2 Rd2 28. a4 a5 29. Kg3 Kf8 30. Nf4 Ke7 31. Rd3 Rb2 "
+        "32. f3 g5 33. hxg5 hxg5 34. Nh3 f6 35. Nf2 f5 36. Nd1 Rc2 37. Nc3 g4 38. "
+        "Nb5 gxf3 39. gxf3 e5 40. Nd6 f4+ 41. Kg4 Bc6 42. Kf5 Re2 43. Nc8+ Ke8 44. "
+        "Nxb6 e4 45. fxe4 Bxe4+ 46. Kxf4 Bxd3 47. Nd5 Rb2 48. c5 Rxb3 49. c6 Kd8 50. "
+        "Ke5 Bc4 51. Nf6 Kc7 52. Kd4 Kxc6 53. Kxc4 Rb4+ 54. Kc3 Rxa4 55. Nh5 Rg4 56. "
+        "Nf6 Rh4 57. Ne4 Kb5 58. Nd6+ Kc6 59. Nc4 a4 60. Ne5+ Kb5 61. Nd3 1-0"
+    )
+    expected_final = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 100, 2, 0, 0, 0, 0],
+            [-1, 0, 0, 0, 0, 0, 0, -5],
+            [0, -100, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
         ]
     )
     pgn_final_position(pgn_moves, expected_final)
